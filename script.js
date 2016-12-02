c64d37da31c8d98cef642a76ad649012193c7d28
Nicolai <nicolai@mmg.io>
2016-12-02 12:34:50 +0000

    Learning about variables

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ alert("Hello from Nicolai");
 document.write("<h2> Welcome to JavaScript Basics </h2>");
 alert("Thanks for visiting ya hozer");
 console.log("program complete!");
+var message = "Hello!";
+alert(message);
